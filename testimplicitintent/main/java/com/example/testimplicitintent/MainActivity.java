141be5978c99d3d66cac2a02d56a88480458e059
Y174655 <33148213+Y174655@users.noreply.github.com>
2020-11-22 23:11:51 +0800

    Add files via upload

---
+++ testimplicitintent/main/java/com/example/testimplicitintent/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.testimplicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener((View v) -> this.startOtherActivity());
+    }
+
+    private void startOtherActivity() {
+        Intent intent = new Intent("com.example.ImplicitActivity");
+        intent.addCategory("android.intent.category.DEFAULT");
+        startActivity(intent);
+    }
+}
